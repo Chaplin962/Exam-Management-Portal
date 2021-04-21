8e2fb2a42312ad68b074b85c736a448ca582e7ae
Abhishek Arya <toabhishekarya@gmail.com>
2021-04-21 09:30:55 +0530

    remote server commit 21-04-21

--- db.php
+++ db.php
@@ -1,14 +1,27 @@
 <?php
+    /*Offline Development
     $servername='localhost';
     $username='root';
     $password='';
     $dbname = 'quiz';
     $con=mysqli_connect($servername,$username,$password,$dbname);
-
+        
     $servername2='localhost';
     $username2='root';
     $password2='';
     $dbname2 = 'simp1';
+    $con2=mysqli_connect($servername2,$username2,$password2,$dbname2);*/
+
+    $servername='remotemysql.com';
+    $username='Jo8QJ7efP6';
+    $password='bCqMAklGyz';
+    $dbname = 'Jo8QJ7efP6';
+    $con=mysqli_connect($servername,$username,$password,$dbname);
+
+    $servername2='remotemysql.com';
+    $username2='Jo8QJ7efP6';
+    $password2='bCqMAklGyz';
+    $dbname2 = 'Jo8QJ7efP6';
     $con2=mysqli_connect($servername2,$username2,$password2,$dbname2);
 
  if(!$con){die('Could not Connect MySql Server :' .mysql_error());}
