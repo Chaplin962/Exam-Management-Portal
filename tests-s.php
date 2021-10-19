667d840dee14ba64a238df988e7084eeb1ffba7b
Abhishek Arya <toabhishekarya@gmail.com>
2021-10-19 12:06:17 +0530

    tab switching trial

--- tests-s.php
+++ tests-s.php
@@ -145,6 +145,16 @@ echo '
 function myFunction() {
   alert("The test is not accessible.");
 }
+
+let siteTitle = '';
+window.addEventListener('blur', () => {
+  siteTitle = document.title;
+  document.title = 'Come back! :c';
+});
+
+window.addEventListener('focus', () => {
+  document.title = siteTitle;
+});
 </script>
 
 <!-- Javascript file 2 -->
